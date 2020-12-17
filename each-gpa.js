7c2e8135bfab12a9b0d2857e823724198d1ca406
dramastore <georgehmariki@gmail.com>
2020-12-18 02:24:50 +0300

    Succed

--- each-gpa.js
+++ each-gpa.js
@@ -1,22 +1,3 @@
-var totalC1 = document.getElementById('total-1').value
-var totalC2 = document.getElementById('total-2').value
-var totalC3 = document.getElementById('total-3').value
-var totalC4 = document.getElementById('total-4').value
-var totalC5 = document.getElementById('total-5').value
-var totalC6 = document.getElementById('total-6').value
-var totalC7 = document.getElementById('total-7').value
-var totalC8 = document.getElementById('total-8').value
-var totalC9 = document.getElementById('total-9').value
-var totalC10 = document.getElementById('total-10').value
-var totalC11 = document.getElementById('total-11').value
-
-var koziMarks = {
-    koziMarks1: {
-        courseWork: Number(document.getElementById('input-course-work1').value),
-        ue: Number(document.getElementById('input-final1').value)
-    }
-}
-
 var calculateGpaButton = {
     btnForFiveCourses: document.getElementById('gpa-btn1'),
     btnForSixCourses: document.getElementById('gpa-btn2'),
@@ -28,6 +9,21 @@ var calculateGpaButton = {
 }
 
 calculateGpaButton.btnForFiveCourses.addEventListener('click', () => {
-    var totalMarksForCourse1 = koziMarks.koziMarks1.courseWork + koziMarks.koziMarks1.ue
-    alert(totalMarksForCourse1)
+    var courseWorkValue = document.getElementById('input-course-work1').valueAsNumber
+    var ueValue = document.getElementById('input-final1').valueAsNumber
+    var totalMarksForCourse = courseWorkValue + ueValue
+
+    if (totalMarksForCourse >= 70) {
+        var YforA = ((0.02 * totalMarksForCourse) + 3).toFixed(1)
+        alert ('GPA =' + ' ' + YforA)
+    }
+    else if (totalMarksForCourse >= 65) {
+        var YforB = ((0.08 * totalMarksForCourse) - 1.2).toFixed(1)
+        alert ('GPA =' + ' ' + YforB)
+    }
+    else if (totalMarksForCourse >= 60) {
+        var YforC = ((0.2 * totalMarksForCourse) - 9).toFixed(1)
+        alert ('GPA =' + ' ' + YforC)
+    }
 })
+
